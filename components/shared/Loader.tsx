c75f7271e8b98923a4e22b41f082890e337fb445
Kamal Preet Singh <kamalpreetsingh025@gmail.com>
2024-04-21 18:31:00 +0530

    Loading Screen

---
+++ components/shared/Loader.tsx
@@ -0,0 +1,9 @@
+const Loader = () => {
+  return (
+    <div className="w-full h-[100vh] flex justify-center items-center">
+      <div className="dot-spin">Loader</div>
+    </div>
+  );
+};
+
+export default Loader;
